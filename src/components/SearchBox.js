8ef1db6bf6fa58f42d1f648d1fa9eccc7d9d5df1
Nguyen Doan <nguyendoan3456@gmail.com>
2024-02-03 07:21:12 -0500

    remove console log

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -58,7 +58,6 @@ export const SearchBox = () => {
   const dispatch = useDispatch();
 
   const onSubmit = (data) => {
-    console.log(data);
     dispatch(searchQuery(data.search));
     dispatch(changePage(1));
   };
